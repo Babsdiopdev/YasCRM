6b167e2c05db13fc70e430078dcae3a6b6d38f40
Issa Sow <issasow25@gmail.com>
2022-07-06 11:52:36 +0000

    mis à jour arboresence du projet

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactComponent } from './components/contact/contact.component';
+import { ContactComponent } from './components/crm/contact/contact.component';
 
 const routes: Routes = [
   { path:'', component: ContactComponent },
--- src/app/serices/contact.service.ts
+++ src/app/serices/contact.service.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Contact } from '../modules/contact.model';
 
 @Injectable({
   providedIn: 'root'
